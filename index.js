27460160c0388db5756d0f1012f2eb489a80ac1e
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-05-16 17:29:10 +0100

    commit tree 1d6bed3b7d4f80945190880a2b4890f51ca7dfc2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser= "Angus";
+
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${'Angus'.slice(0,1)}!`;
